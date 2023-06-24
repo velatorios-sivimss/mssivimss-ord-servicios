473775baf3c94d1400585719e7af7eea721e93b4
Pablo Nolasco Castro <pablo.nolasco@people-media.com.mx>
2023-06-23 20:46:27 -0500

    Feat: add ataudes

--- src/main/java/com/imss/sivimss/ordservicios/beans/Ataud.java
+++ src/main/java/com/imss/sivimss/ordservicios/beans/Ataud.java
@@ -34,20 +34,35 @@ public class Ataud {
 	public DatosRequest obtenerAtaudes() {
 		DatosRequest request = new DatosRequest();
 		Map<String, Object>paramtero= new HashMap<>();
-		SelectQueryUtil selectQueryUtil= new SelectQueryUtil();
-		selectQueryUtil.select("SA.ID_ARTICULO AS idArticulo, SA.DES_MODELO_ARTICULO AS nombreArticulo")
-		.from("SVT_ARTICULO SA")
-		.innerJoin("SVC_CATEGORIA_ARTICULO SCA", "SA.ID_CATEGORIA_ARTICULO = SCA.ID_CATEGORIA_ARTICULO")
-		.innerJoin("SVC_TIPO_ARTICULO STA", "SA .ID_TIPO_ARTICULO =STA.ID_TIPO_ARTICULO")
-		.where("SA.ID_CATEGORIA_ARTICULO = 1")
-		.and("SA.CAN_UNIDAD > 0")
-		.and("SA.IND_ACTIVO = 1")
-		.and("SA.ID_TIPO_ARTICULO =1");
-		String query= selectQueryUtil.build();
+		SelectQueryUtil selectQueryUtilArticulo= new SelectQueryUtil();
+		SelectQueryUtil selectQueryUtilInventarioTemp= new SelectQueryUtil();
+		SelectQueryUtil selectQueryUtilInventario= new SelectQueryUtil();
+		
+		selectQueryUtilInventarioTemp.select("STP.ID_INVE_ARTICULO")
+		.from("SVC_DETALLE_CARACTERISTICAS_PRESUPUESTO_TEMP STP")
+		.where("STP.IND_ACTIVO=1")
+		.and("DATE_FORMAT(STP.FEC_ALTA,'YY-MM-DD')=DATE_FORMAT(CURRENT_DATE(),'YY-MM-DD')")
+		.and("TIMESTAMPDIFF(MINUTE,DATE_ADD(STP.FEC_ALTA, INTERVAL 4 HOUR),CURRENT_TIMESTAMP()) <= 0");
+		
+		selectQueryUtilInventario.select("SDCP.ID_INVE_ARTICULO")
+		.from("SVC_DETALLE_CARACTERISTICAS_PRESUPUESTO SDCP")
+		.where("SDCP.IND_ACTIVO=1");
+		
+		selectQueryUtilArticulo.select("STI.ID_INVE_ARTICULO","STI.FOLIO_ARTICULO")
+		.from("SVT_INVENTARIO_ARTICULO STI")
+		.innerJoin("SVT_ORDEN_ENTRADA SOE2", "SOE2.ID_ODE = STI.ID_ODE")
+		.innerJoin("SVT_CONTRATO SC", "SC.ID_CONTRATO = SOE2.ID_CONTRATO")
+		.innerJoin("SVT_PROVEEDOR SP", "SP .ID_PROVEEDOR = SC.ID_PROVEEDOR")
+		.where("STI.ID_ARTICULO=1 AND STI.IND_ESTATUS NOT IN (2,3)")
+		.and("STI.ID_TIPO_ASIGNACION_ART =1")
+		.and("STI.ID_INVE_ARTICULO NOT IN ("+selectQueryUtilInventarioTemp.build()+")")
+		.and("STI.ID_INVE_ARTICULO NOT IN("+selectQueryUtilInventario.build()+")");
+		
+		String query=selectQueryUtilArticulo.build();
 		
 		log.info(query);
-
 		String encoded= DatatypeConverter.printBase64Binary(query.getBytes(StandardCharsets.UTF_8));
+
 		paramtero.put(AppConstantes.QUERY, encoded);
 		request.setDatos(paramtero);
 		return request;
