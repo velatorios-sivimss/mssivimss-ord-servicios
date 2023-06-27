e90dc17966cfa7e01b6a9896de79c10df40ad5ec
Pablo Nolasco Castro <pablo.nolasco@people-media.com.mx>
2023-06-27 12:06:47 -0500

    Fix: consulta proveedores

--- src/main/java/com/imss/sivimss/ordservicios/beans/Servicio.java
+++ src/main/java/com/imss/sivimss/ordservicios/beans/Servicio.java
@@ -33,12 +33,13 @@ public class Servicio {
 		DatosRequest datosRequest= new DatosRequest();
 		Map<String, Object>parametros= new HashMap<>();
 		SelectQueryUtil selectQueryUtil= new SelectQueryUtil();
-		selectQueryUtil.select("SS.ID_SERVICIO AS idServicio","SS.DES_NOM_SERVICIO AS nombreServicio")
+		selectQueryUtil.select("DISTINCT SS.ID_SERVICIO AS idServicio","SS.DES_NOM_SERVICIO AS nombreServicio")
 		.from("SVT_SERVICIO SS")
 		.innerJoin("SVT_CONTRATO_SERVICIO SCS", "SS.ID_SERVICIO = SCS.ID_SERVICIO")
 		.innerJoin("SVT_CONTRATO SC", "SCS.ID_CONTRATO =SC.ID_CONTRATO")
 		.where("SS.IND_ACTIVO =1")
-		.and("DATE_FORMAT(SC.FEC_FIN_VIG,\"%Y-%M-%D\") <= DATE_FORMAT(CURRENT_DATE(),\"%Y-%M-%D\")")
+		.and("SC.IND_ACTIVO=1")
+		.and("DATE_FORMAT(SC.FEC_FIN_VIG,\"YY-MM-DD\") >= DATE_FORMAT(CURRENT_DATE(),\"YY-MM-DD\")")
 		.orderBy("nombreServicio ASC");
 		
 		String query=selectQueryUtil.build();
@@ -61,10 +62,10 @@ public class Servicio {
 		.innerJoin("SVT_CONTRATO_SERVICIO SCS", "SC.ID_CONTRATO = SCS.ID_CONTRATO")
 		.innerJoin("SVT_SERVICIO SS", "SCS.ID_SERVICIO = SS.ID_SERVICIO")
 		.where("SP.IND_ACTIVO =1")
-		.and("SC.IND_ACTIVO =1")
+		.and("SC.IND_ACTIVO=1")
 		.and("SS.ID_SERVICIO = "+idServicio)
-		.and("DATE_FORMAT(SC.FEC_FIN_VIG,\"%Y-%M-%D\") <= DATE_FORMAT(CURRENT_DATE(),\"%Y-%M-%D\")")
-		.and("SP.FEC_VIGENCIA >= CURRENT_DATE() ")
+		.and("DATE_FORMAT(SC.FEC_FIN_VIG,\"YY-MM-DD\") >= DATE_FORMAT(CURRENT_DATE(),\"YY-MM-DD\")")
+		.and("DATE_FORMAT(SP.FEC_VIGENCIA,\"YY-MM-DD\") >= DATE_FORMAT(CURRENT_DATE(),\"YY-MM-DD\")")
 		.orderBy("SP.NOM_PROVEEDOR ASC");
 		
 		String query=selectQueryUtil.build();
